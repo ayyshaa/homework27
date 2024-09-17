b365b161c9e3a00cf5c73a6759c002ffea696a5a
ayyshaa <qacarovaayse@gmail.com>
2024-09-17 18:56:23 +0400

    arr

---
+++ app.js
@@ -0,0 +1,22 @@
+let arr = ["qırmızı", "mavi", "yaşıl", "sarı"];
+arr.push("yasil");
+console.log(arr);
+ 
+
+
+let arr1 = ["qırmızı", "mavi", "yaşıl", "sarı"];
+arr1.pop();
+console.log(arr1);
+
+let arr2 = ["qırmızı", "mavi", "yaşıl", "sarı"];
+arr2.unshift("yasil");
+console.log(arr2);
+
+let arr3 = ["qırmızı", "mavi", "yaşıl", "sarı"];
+arr3.shift();
+console.log(arr3);
+
+let arr4 = ["qırmızı", "mavi", "yaşıl", "sarı"];
+for(let i = 0;i < arr4.length; i++){
+    console.log(arr4[i]);
+}
